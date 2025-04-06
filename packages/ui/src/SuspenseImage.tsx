e2c73cc473301567e6f21b59a62b6110701acfcd
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2025-04-06 20:14:31 +0200

    feat(SuspenseImage): enhance imageLoader to accept dynamic width and quality parameters

--- packages/ui/src/SuspenseImage.tsx
+++ packages/ui/src/SuspenseImage.tsx
@@ -10,15 +10,15 @@ const imageCache = new Set<string>();
 const DefaultWidth = 640 satisfies ImageLoaderProps['width'];
 const DefaultQuality = 75 satisfies ImageLoaderProps['quality'];
 
-function imageLoader({src}: {src: ImageLoaderProps['src']}) {
+function imageLoader({src, width, quality = DefaultQuality}: ImageLoaderProps) {
   const url = new URL('/_next/image', window.location.origin);
   url.searchParams.set('url', src);
-  url.searchParams.set('w', String(DefaultWidth));
-  url.searchParams.set('q', String(DefaultQuality));
+  url.searchParams.set('w', String(width));
+  url.searchParams.set('q', String(quality));
   return String(url);
 }
 
-function useSuspenseImage(props: {src: ImageProps['src']}) {
+function useSuspenseImage(props: Pick<ImageProps, 'src' | 'quality'>) {
   if (!isBrowserEnvironment) {
     throw new Error('SuspenseImage can only be used in the browser.');
   }
@@ -26,7 +26,11 @@ function useSuspenseImage(props: {src: ImageProps['src']}) {
   if (!imageCache.has(src)) {
     const {promise, resolve, reject} = Promise.withResolvers();
     const img = new Image();
-    img.src = imageLoader({src});
+    img.src = imageLoader({
+      src,
+      width: DefaultWidth,
+      ...(props.quality && {quality: Number(props.quality)}),
+    });
     img.onload = () => {
       imageCache.add(src);
       resolve(null);
@@ -41,7 +45,14 @@ function useSuspenseImage(props: {src: ImageProps['src']}) {
 
 export function SuspenseImage(props: ImageProps) {
   useSuspenseImage(props);
-  return <NextImage loader={imageLoader} {...props} />;
+  return (
+    <NextImage
+      loader={(imageLoaderProps) =>
+        imageLoader({...imageLoaderProps, width: DefaultWidth})
+      }
+      {...props}
+    />
+  );
 }
 
 function resolveImageSrc(src: ImageProps['src']) {
