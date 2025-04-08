b6770bae9e2829e09e7cb67af0daf4743e2f8a34
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-04-08 22:10:35 +0200

    feat(ui): enhance Footer component with policy links and improved layout

--- apps/web/app/(account)/_components/Footer.tsx
+++ apps/web/app/(account)/_components/Footer.tsx
@@ -1,9 +1,27 @@
+import {TextBlock} from '@repo/ui/TextBlock';
 import {baseColors} from '@repo/ui/variables/colors.stylex';
-import {borderWidth} from '@repo/ui/variables/tokens.stylex';
+import {borderWidth, spacing} from '@repo/ui/variables/tokens.stylex';
 import * as stylex from '@stylexjs/stylex';
 
 export function Footer() {
-  return <footer {...stylex.props(styles.base)}></footer>;
+  return (
+    <footer {...stylex.props(styles.base)}>
+      <ul {...stylex.props(listStyles.base)}>
+        <li>
+          <TextBlock appearance="accent">Refund Policy</TextBlock>
+        </li>
+        <li>
+          <TextBlock appearance="accent">Shipping Policy</TextBlock>
+        </li>
+        <li>
+          <TextBlock appearance="accent">Privacy Policy</TextBlock>
+        </li>
+        <li>
+          <TextBlock appearance="accent">Terms of Service</TextBlock>
+        </li>
+      </ul>
+    </footer>
+  );
 }
 
 const styles = stylex.create({
@@ -12,5 +30,15 @@ const styles = stylex.create({
     borderBlockStartWidth: borderWidth.base,
     borderBlockStartStyle: 'solid',
     borderBlockStartColor: baseColors.border,
+    paddingBlock: spacing.large200,
+  },
+});
+
+const listStyles = stylex.create({
+  base: {
+    display: 'flex',
+    gap: spacing.large200,
+    listStyleType: 'none',
+    padding: spacing.none,
   },
 });
