b488b7fede1ad3093cb02833458ea4957b07d96f
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-04-11 13:55:13 +0200

    refactor(ui): replace Fragment with shorthand syntax in OrdersPage and ProfilePage components

--- apps/web/app/(account)/orders/page.tsx
+++ apps/web/app/(account)/orders/page.tsx
@@ -1,16 +1,15 @@
 import {baseColors} from '@repo/ui/variables/colors.stylex';
 import {spacing} from '@repo/ui/variables/tokens.stylex';
 import * as stylex from '@stylexjs/stylex';
-import {Fragment} from 'react';
 
 import {PageTitle} from '../_components/PageTitle';
 
 export default function OrdersPage() {
   return (
-    <Fragment>
+    <>
       <PageTitle>Orders</PageTitle>
       <TabGroup />
-    </Fragment>
+    </>
   );
 }
 
--- apps/web/app/(account)/profile/page.tsx
+++ apps/web/app/(account)/profile/page.tsx
@@ -1,11 +1,9 @@
-import {Fragment} from 'react';
-
 import {PageTitle} from '../_components/PageTitle';
 
 export default function ProfilePage() {
   return (
-    <Fragment>
+    <>
       <PageTitle>Profile</PageTitle>
-    </Fragment>
+    </>
   );
 }
