c971a515c6c7c7742c98d5c0da5066d85f98f5e5
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-05-08 10:26:22 +0200

    refactor: simplify SuspenseImage component by removing unused code and improving image loading logic

--- packages/ui/src/SuspenseImage.tsx
+++ packages/ui/src/SuspenseImage.tsx
@@ -1,65 +1,31 @@
 'use client';
-import type {ImageLoaderProps, ImageProps, StaticImageData} from 'next/image';
+
+import {getImageProps, type ImageProps} from 'next/image';
+import {use} from 'react';
 import {useIsSSR} from 'react-aria';
 
 import {Image} from './Image';
 
-const DefaultWidth = 640 satisfies ImageLoaderProps['width'];
-const DefaultQuality = 75 satisfies ImageLoaderProps['quality'];
-
-const imageCache = new Set<string>();
+const imagePromises = new Map<string, Promise<void>>();
 
 export function SuspenseImage(props: ImageProps) {
-  const isSSR = useIsSSR();
-  if (isSSR) {
-    throw new Error('SuspenseImage can only be used in the browser.');
+  if (useIsSSR()) {
+    throw new Error('SuspenseImage must be used in a client component');
   }
-  const src = getImageSrc(props.src);
-  if (!imageCache.has(src)) {
-    const {promise, resolve, reject} = Promise.withResolvers();
+  const {src} = getImageProps(props).props;
+
+  let imagePromise = imagePromises.get(src);
+  if (!imagePromise) {
+    const {promise, resolve, reject} = Promise.withResolvers<void>();
+
     const img = new window.Image();
-    img.src = imageLoader({
-      src,
-      width: DefaultWidth,
-      ...(props.quality && {quality: Number(props.quality)}),
-    });
-    img.onload = () => {
-      imageCache.add(src);
-      resolve(null);
-    };
-    img.onerror = () => {
-      imageCache.add(src);
-      reject(new Error(`Failed to load image: ${src}`));
-    };
-    throw promise;
-  }
-  return (
-    <Image
-      loader={imageLoaderProps =>
-        imageLoader({...imageLoaderProps, width: DefaultWidth})
-      }
-      {...props}
-    />
-  );
-}
+    img.src = src;
+    img.onload = () => resolve();
+    img.onerror = () => reject();
 
-function imageLoader({src, width, quality = DefaultQuality}: ImageLoaderProps) {
-  const url = new URL('/_next/image', window.location.origin);
-  url.searchParams.set('url', src);
-  url.searchParams.set('w', String(width));
-  url.searchParams.set('q', String(quality));
-  return String(url);
-}
+    imagePromises.set(src, (imagePromise = promise));
+  }
+  use(imagePromise);
 
-function getImageSrc(src: ImageProps['src']) {
-  if (typeof src === 'string') {
-    return src;
-  }
-  let staticImageData: StaticImageData;
-  if ('default' in src) {
-    staticImageData = src.default;
-  } else {
-    staticImageData = src;
-  }
-  return staticImageData.src;
+  return <Image {...props} />;
 }
