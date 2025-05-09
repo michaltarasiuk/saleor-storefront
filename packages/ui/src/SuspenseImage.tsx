ae685be9539a893ab45666106c4038e963fd93e1
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-05-09 14:45:05 +0200

    refactor: update SuspenseImage component to use client-only and remove SSR check

--- packages/ui/src/SuspenseImage.tsx
+++ packages/ui/src/SuspenseImage.tsx
@@ -1,31 +1,23 @@
-'use client';
+import 'client-only';
 
 import {getImageProps, type ImageProps} from 'next/image';
 import {use} from 'react';
-import {useIsSSR} from 'react-aria';
 
 import {Image} from './Image';
 
 const imagePromises = new Map<string, Promise<void>>();
 
 export function SuspenseImage(props: ImageProps) {
-  if (useIsSSR()) {
-    throw new Error('SuspenseImage must be used in a client component');
-  }
   const {src} = getImageProps(props).props;
-
   let imagePromise = imagePromises.get(src);
   if (!imagePromise) {
     const {promise, resolve, reject} = Promise.withResolvers<void>();
-
     const img = new window.Image();
     img.src = src;
     img.onload = () => resolve();
     img.onerror = () => reject();
-
     imagePromises.set(src, (imagePromise = promise));
   }
   use(imagePromise);
-
   return <Image {...props} />;
 }
